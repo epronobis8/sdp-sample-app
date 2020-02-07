243207afa99a661a1bdb5ae83560a1cef8cbad65
608596 <erinpronobis@bah5cd8340yph.resource.ds.bah.com>
2020-02-07 12:29:54 -0500

    re-did pipeline brackets

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,17 +1,15 @@
 libraries{
   sdp{
-      protractor {
+    protractor {
      url = "localhost:9000"
      enforce = true
      config_file = "protractor.conf.js"
     }
-  
     images{
       registry = "http://0.0.0.0:5000" // registry url
       cred = "sdp-docker-registry"// jenkins cred id to authenticate
       docker_args = "--network=try-it-out_sdp"  // docker runtime args
     }
-  } 
   }
   github_enterprise
   sonarqube{
