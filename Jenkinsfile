4f147e53c0be6d4d3f6146011bcaf5248bc5130f
608596 <erinpronobis@c02c21c2md6v.users.bah.com>
2020-02-10 13:34:14 -0500

    may break pipeline. added two checks

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,3 +1,4 @@
 static_dependency_check_analysis()
 build()
 static_code_analysis()
+penetration_test()
--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -5,20 +5,22 @@ libraries{
       registry = "http://0.0.0.0:5000" // registry url
       cred = "sdp-docker-registry"// jenkins cred id to authenticate
       docker_args = "--network=try-it-out_sdp"  // docker runtime args
-
-      owasp_dep_check {
-      scan_target = "src"
-      cvss_threshold = "9"
-    }
     }
   }
   github_enterprise
   sonarqube{
     enforce_quality_gate = true
-    
   }
+  owasp_dep_check {
+      scan_target = "src"
+      cvss_threshold = "9"
+    }
   docker{
     registry = "0.0.0.0:5000"
     cred = "sdp-docker-registry"
+  }
+    owasp_zap{
+    target = "0.0.0.0:5000"
+    vulnerability_threshold = "Low"
   }
 }
