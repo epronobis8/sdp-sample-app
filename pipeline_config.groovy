49a575fcb823cde306ab8a68a03a7533bd4cca7a
608596 <erinpronobis@c02c21c2md6v.users.bah.com>
2020-02-10 12:52:34 -0500

    changed to under libraries.

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -5,6 +5,11 @@ libraries{
       registry = "http://0.0.0.0:5000" // registry url
       cred = "sdp-docker-registry"// jenkins cred id to authenticate
       docker_args = "--network=try-it-out_sdp"  // docker runtime args
+
+      owasp_dep_check {
+      scan_target = "src"
+      cvss_threshold = "9"
+    }
     }
   }
   github_enterprise
@@ -15,9 +20,5 @@ libraries{
   docker{
     registry = "0.0.0.0:5000"
     cred = "sdp-docker-registry"
-  }
-  owasp_dep_check {
-    scan_target = "/var/jenkins_home/workspace/sample-spring-boot-api_master/"
-    cvss_threshold = "9"
   }
 }
