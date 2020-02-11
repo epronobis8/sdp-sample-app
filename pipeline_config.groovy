29ae3ab72a99efab17e96a2978a21361c94527d7
608596 <erinpronobis@c02c21c2md6v.users.bah.com>
2020-02-11 09:46:44 -0500

    changed to github repo to remove git

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -16,7 +16,7 @@ libraries{
     cred = "sdp-docker-registry"
   }
     owasp_dep_check {
-    scan_target = "https://github.com/epronobis8/sdp-sample-app.git"
+    scan_target = "https://github.com/epronobis8/sdp-sample-app"
     cvss_threshold = "9"
   }
 }
