fd6c87d7b1ad97bb5927a5935f94ef5702e256d3
608596 <erinpronobis@c02c21c2md6v.users.bah.com>
2020-02-11 09:49:01 -0500

    changed order of pipeline

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -16,7 +16,7 @@ libraries{
     cred = "sdp-docker-registry"
   }
     owasp_dep_check {
-    scan_target = "https://github.com/epronobis8/sdp-sample-app"
+    scan_target = "0.0.0.0:5000"
     cvss_threshold = "9"
   }
 }
